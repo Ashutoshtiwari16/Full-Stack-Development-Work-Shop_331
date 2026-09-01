104042894a20ea85df78182961e9a8f1570a4b32
Ashutoshtiwari16 <ashutoshtiwariayc@gmail.com>
2026-09-01 10:55:20 +0530

    DONE

--- backend/LAB3/httpserver.js
+++ backend/LAB3/httpserver.js
@@ -1,20 +1,41 @@
 import http from "http";
-
+    const Port=3005;
+    const userdata=[{id:1,name:"abc",email:"c.m@Abes.ac.com"}];
 const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
 
-    if (url === "/msg" && method === "GET") {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/plain");
-        res.end("Hello Server.");
-    } else {
-        res.statusCode = 404;
-        res.setHeader("Content-Type", "text/plain");
-        res.end("Page Not Found");
+    if (url === "/msg" && method == "GET") {
+        res.statusCode = 201;
+        // res.setHeader("Content-Type", "text/plain");
+        res.end("Welcome to user.");
+    } else if( url=="/sys" && method == "GET") {
+        res.statusCode = 201;
+        // res.setHeader("Content-Type", "text/plain");
+        res.end("This is system information");
+    }
+    else if (url == "/data" && method == "GET"){
+        res.end(JSON.stringify(userdata));
+    }
+    else if(url=="/create" && method == "POST"){
+        const body =" ";
+        req.on("data",(chunk)=>{
+            body+=chunk;
+        })
+        req.on("end",()=>{
+            const newdata =JSON.parse(body);
+            const newUserdata = {
+                id:newdata.id,
+                name:newdata.name,
+                email:newdata.email,
+            }
+            userdata.push(newuserdata)
+            res.end("datacreatedforcefully")
+            
+        })
     }
 });
 
-server.listen(3000, () => {
-    console.log("Server is running on port number 3000");
+server.listen(Port, () => {
+    console.log(`Server is running on port number ${Port}`);
 });
